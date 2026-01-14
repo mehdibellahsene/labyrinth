d1268d79292ee95412e5e85f526233453a4624ce
BELLAHSENE Mehdi <m.bellahsene@office64.fr>
2026-01-14 21:52:52 +0100

    feat: add minimal API endpoints for training server

--- LabyrinthServer/Program.cs
+++ LabyrinthServer/Program.cs
@@ -1,6 +1,42 @@
+using LabyrinthServer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ILabyrinthService, LabyrinthService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+var api = app.MapGroup("/api");
+
+api.MapGet("/maze", (ILabyrinthService svc) => Results.Ok(svc.GetMaze()));
+
+api.MapPost("/crawler", (ILabyrinthService svc) =>
+{
+    var id = svc.CreateCrawler();
+    return Results.Created($"/api/crawler/{id}", new { id });
+});
+
+api.MapGet("/crawler/{id}/facing", (int id, ILabyrinthService svc) =>
+{
+    var tile = svc.GetFacingTile(id);
+    return tile is null ? Results.NotFound() : Results.Ok(tile);
+});
+
+api.MapPost("/crawler/{id}/turn", (int id, bool left, ILabyrinthService svc) =>
+    svc.TurnCrawler(id, left) ? Results.Ok() : Results.NotFound());
+
+api.MapPost("/crawler/{id}/walk", (int id, ILabyrinthService svc) =>
+    Results.Ok(svc.TryWalk(id)));
+
+api.MapGet("/crawler/{id}/inventory", (int id, ILabyrinthService svc) =>
+{
+    var inv = svc.GetInventory(id);
+    return inv is null ? Results.NotFound() : Results.Ok(inv);
+});
 
 app.Run();
