c58da01d9a5547e832089eb22b4d27ca1ff7200c
BELLAHSENE Mehdi <m.bellahsene@office64.fr>
2026-01-14 21:47:19 +0100

    feat: define IExplorationStrategy interface

---
+++ Labyrinth/Exploration/IExplorationStrategy.cs
@@ -0,0 +1,11 @@
+namespace Labyrinth.Exploration;
+
+using Labyrinth.Core;
+
+public interface IExplorationStrategy
+{
+    Task<Actions> GetNextActionAsync(ICrawler crawler, CancellationToken ct = default);
+    void Reset();
+}
+
+public enum Actions { TurnLeft, TurnRight, Walk }
