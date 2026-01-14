75eb6415d1ffcb8752e5a33090faf945bbd5dc72
BELLAHSENE Mehdi <m.bellahsene@office64.fr>
2026-01-14 21:51:49 +0100

    feat: add server models for crawler state

---
+++ LabyrinthServer/Models/CrawlerState.cs
@@ -0,0 +1,15 @@
+namespace LabyrinthServer.Models;
+
+using Labyrinth.Core;
+
+public class CrawlerState
+{
+    public int Id { get; init; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Direction Direction { get; set; }
+    public List<string> Inventory { get; } = new();
+}
+
+public record WalkResult(bool Success, IEnumerable<string>? Items = null);
+public record TileInfo(string Type);
